47953bb3d20fe85ce6f09083d4fb558d3cce4bfa
sayid <herq12315@gmail.com>
2018-09-13 16:18:57 +0800

    get Station code and README

---
+++ README.md
@@ -0,0 +1,84 @@
+Python 知识点
+
+判断一个元素是否在 list 中
+```
+a = [1, 2.5, 7, 13221, 4.6545]
+if 7 in a:
+    print('yes')
+else:
+    print('no')
+```
+
+判断是否是数字
+```
+try:
+   val = int(userInput)
+except ValueError:
+   print("That's not an int!")
+```
+
+判断是什么类型
+```
+>>> type([]) is list
+True
+>>> type({}) is dict
+True
+>>> type('') is str
+True
+>>> type(0) is int
+True
+>>> type({})
+<type 'dict'>
+>>> type([])
+<type 'list'>
+```
+
+Python range() 函数用法
+
+```
+range(start, stop[, step])
+
+>>>range(10)        # 从 0 开始到 10
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+>>> range(1, 11)     # 从 1 开始到 11
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+>>> range(0, 30, 5)  # 步长为 5
+[0, 5, 10, 15, 20, 25]
+>>> range(0, 10, 3)  # 步长为 3
+[0, 3, 6, 9]
+>>> range(0, -10, -1) # 负数
+[0, -1, -2, -3, -4, -5, -6, -7, -8, -9]
+>>> range(0)
+[]
+>>> range(1, 0)
+[]
+```
+以下是 range 在 for 中的使用，循环出runoob 的每个字母:
+```
+>>>x = 'runoob'
+>>> for i in range(len(x)) :
+...     print(x[i])
+...
+r
+u
+n
+o
+o
+b
+>>>
+```
+
+If 在列表中
+```
+ if number in range(len(response)):
+            print('yes')
+        else:
+            print('no')
+```
+
+Input text
+```
+in Python 3.x:
+
+filename = input('Enter a file name: ')
+```
--- getStationCode.py
+++ getStationCode.py
@@ -3,12 +3,51 @@ import requests
 token = '2d20b5f834a90f9a1af1463bc9114a477f96a50c'
 city = 'shanghai'
 
+def showCityInfo(response):
+    for cityNum in range(len(response)):
+        print(cityNum+1, response[cityNum]['uid'], response[cityNum]['aqi'], response[cityNum]['station']['name'])
+
+
+def getInput(response, token):
+    inputNum  = input('请输入数字: ')
+    print(inputNum)
+
+    try:
+        number = int(inputNum)
+        
+        if number in range(len(response)):
+            print('yes')
+            print(type(response[number]))
+            print(response[number])
+        
+            stationCode = response[number-1]['uid']
+            print("请在 config.json 文件中，把 stationCode 修改为" + str(stationCode))
+
+        else:
+            print('你输入的数字不在列表中')
+            getInput(response, token)
+
+    except ValueError:
+        print("你输入的不是整数")
+        getInput(response, token)
+    
+    
+
 def getJSON(token, city):
     print(token)
     print(city)
     city_url = 'http://api.waqi.info/search/?'
     parameters = {"token": token, "keyword": city}
     r = requests.get(city_url, params=parameters)
-    print(r.json())
+    #print(r.json())
+    response_city = r.json()
+    #print(response_city['data'])
+    print(len(response_city['data']))
+    showCityInfo(response_city['data'])
+    getInput(response_city['data'], token)
+
 
 getJSON(token, city)
+
+
+
