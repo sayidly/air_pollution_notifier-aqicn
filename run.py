7d27a60d3ce8adfb198ff78c0022b944d58006b0
sayid <herq12315@gmail.com>
2018-09-30 16:53:52 +0800

    add costomize menu

--- run.py
+++ run.py
@@ -17,11 +17,23 @@ robot.config['HOST'] = '127.0.0.1'
 robot.config['PORT'] = 8080
 
 #robot.config["APP_ID"] = "wx1f0f6bbb3392da37"
-#robot.config['ENCODING_AES_KEY'] = '0145b463291a78d8a6c03c300fa02c09'
+#robot.config['APP_SECRET'] = '0145b463291a78d8a6c03c300fa02c09'
 
 # testing
 robot.config["APP_ID"] = "wx07910350914a1994"
-robot.config['ENCODING_AES_KEY'] = '7ec65182bd26af5c323c0fa3e4e66c42'
+robot.config['APP_SECRET'] = '7ec65182bd26af5c323c0fa3e4e66c42'
 
+# 自定义菜单
+client = robot.client
+client.create_menu({
+    "button":[{
+         "type": "click",
+         "name": "今日歌曲",
+         "key": "music"
+    }]
+})
+@robot.key_click("music")
+def music(message):
+    return '你点击了“今日歌曲”按钮'
 
 robot.run()
