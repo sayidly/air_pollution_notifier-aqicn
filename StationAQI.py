c59c962503d2782736bc153eafc0332d6ed39b2b
sayid <herq12315@gmail.com>
2018-10-08 15:53:16 +0800

    change process function

--- StationAQI.py
+++ StationAQI.py
@@ -41,35 +41,38 @@ def processData(response):
     result['time'] = data['time']['s']
     result['iaqi'] = data['iaqi']
     print(result)
+
+    def calculateLevel(aqi):
+        level = 0
+        if aqi >= 0 and aqi <= 50:
+            level = 1
+        elif aqi >= 51 and aqi <= 100:
+            level = 2
+        elif aqi >= 101 and aqi <= 150:
+            level = 3
+        elif aqi >= 151 and aqi <= 200:
+            level = 4
+        elif aqi >= 201 and aqi <= 300:
+            level = 5
+        elif aqi > 300:
+            level = 6
+        return level
+
     level = calculateLevel(result['aqi'])
     print(level)
+
+    def selectInfoText(level):
+        if level > 6 or level < 0:
+            level = 0
+        levelInfo = {}
+        levelInfo['value'] = level
+        levelInfo['name'] = info_json['level'][level]['name'] 
+        levelInfo['implication'] = info_json['level'][level]['implication'] 
+        levelInfo['statement'] = info_json['level'][level]['statement'] 
+        return levelInfo
+
     levelInfo = selectInfoText(level)
     result['level'] = levelInfo
     print(result)
     return result
 
-def calculateLevel(aqi):
-    level = 0
-    if aqi >= 0 and aqi <= 50:
-        level = 1
-    elif aqi >= 51 and aqi <= 100:
-        level = 2
-    elif aqi >= 101 and aqi <= 150:
-        level = 3
-    elif aqi >= 151 and aqi <= 200:
-        level = 4
-    elif aqi >= 201 and aqi <= 300:
-        level = 5
-    elif aqi > 300:
-        level = 6
-    return level
-
-def selectInfoText(level):
-    if level > 6 or level < 0:
-        level = 0
-    levelInfo = {}
-    levelInfo['value'] = level
-    levelInfo['name'] = info_json['level'][level]['name'] 
-    levelInfo['implication'] = info_json['level'][level]['implication'] 
-    levelInfo['statement'] = info_json['level'][level]['statement'] 
-    return levelInfo
